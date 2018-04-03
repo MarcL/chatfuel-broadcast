e42cae09e5343a5743017b8b4f0649c73e4b07b6
Marc Littlemore <marc.littlemore@gmail.com>
2018-04-03 22:34:09 +0100

    Add code + tests

---
+++ src/broadcast.js
@@ -0,0 +1,54 @@
+import requestPromise from 'request-promise';
+import url from 'url';
+
+// http://docs.chatfuel.com/broadcasting/broadcasting-documentation/broadcasting-api
+
+const CHATFUEL_BASE_URL = 'https://api.chatfuel.com';
+
+const validateExpectedParameters = (options) => {
+    const expectedParameters = [
+        'botId', 'blockId', 'token', 'userId'
+    ];
+
+    expectedParameters.forEach((parameter) => {
+        if (!options[parameter]) {
+            throw new Error(`Expected ${parameter} to be passed`);
+        }
+    });
+}
+
+const createChatfuelBroadcastUrl = (botId, userId) => {
+    return `${CHATFUEL_BASE_URL}/bots/${botId}/users/${userId}/send`;
+};
+
+const broadcast = (options) => {
+    validateExpectedParameters(options);
+
+    const { botId, blockId, token, userId, attributes } = options;
+
+    const chatfuelBroadcastUrl = createChatfuelBroadcastUrl(botId, userId);
+
+    const query = Object.assign({},
+        {
+            chatfuel_token: token,
+            chatfuel_block_name: blockId
+        },
+        attributes
+    );
+
+    const chatfuelApiUrl = url.format({
+        pathname: chatfuelBroadcastUrl,
+        query
+    });
+
+    const requestOptions = {
+        uri: chatfuelApiUrl,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    return requestPromise.post(requestOptions);
+};
+
+export default broadcast;
---
+++ test/unit/broadcast.test.js
@@ -0,0 +1,96 @@
+import requestPromise from 'request-promise';
+import broadcast from '../../src/broadcast';
+import sinon from 'sinon';
+import { expect } from 'chai';
+
+describe('Chatfuel.broadcast()', () => {
+    let defaultOptions;
+    let stubRequestPromisePost;
+
+    const defaultBotId = 'defaultBotId';
+    const defaultBlocklId = 'defaultBlockId';
+    const defaultToken = 'defaultToken';
+    const defaultUserId = 'defaultUserId';
+
+    beforeEach(() => {
+        defaultOptions = {
+            botId: defaultBotId,
+            blockId: defaultBlocklId,
+            token: defaultToken,
+            userId: defaultUserId,
+            attributes: {}
+        };
+
+        stubRequestPromisePost = sinon.stub(requestPromise, 'post');
+    });
+
+    afterEach(() => {
+        stubRequestPromisePost.restore();
+    });
+
+    describe('should throw expected error', () => {
+        it('when botId is missing', () => {
+            delete defaultOptions.botId;
+            const wrapperFunction = () => broadcast(defaultOptions);
+
+            expect(wrapperFunction).to.throw('Expected botId to be passed');
+        });
+
+        it('when blockId is missing', () => {
+            delete defaultOptions.blockId;
+            const wrapperFunction = () => broadcast(defaultOptions);
+
+            expect(wrapperFunction).to.throw('Expected blockId to be passed');
+        });
+
+        it('when token is missing', () => {
+            delete defaultOptions.token;
+            const wrapperFunction = () => broadcast(defaultOptions);
+
+            expect(wrapperFunction).to.throw('Expected token to be passed');
+        });
+
+        it('when userId is missing', () => {
+            delete defaultOptions.userId;
+            const wrapperFunction = () => broadcast(defaultOptions);
+
+            expect(wrapperFunction).to.throw('Expected userId to be passed');
+        });
+    });
+
+    it('should call expected endpoint', () => {
+        const chatfuelEndpointUrl = `https://api.chatfuel.com/bots/${defaultBotId}/users/${defaultUserId}/send`;
+        const expectedUri = `${chatfuelEndpointUrl}?chatfuel_token=${defaultToken}&chatfuel_block_name=${defaultBlocklId}`;
+
+        broadcast(defaultOptions);
+
+        expect(stubRequestPromisePost.getCall(0).args[0].uri).to.equal(expectedUri);
+    });
+
+    it('should set expected request headers', () => {
+        const expectedRequestHeaders = {
+            'Content-Type': 'application/json'
+        };
+
+        broadcast(defaultOptions);
+
+        expect(stubRequestPromisePost.getCall(0).args[0].headers)
+            .to.deep.equal(expectedRequestHeaders);
+    });
+
+    it('should append passed attributes as endpoint query parameters', () => {
+        const givenAttributes = {
+            fakeattribute1: 'fakeAttribute1',
+            fakeattribute2: 'fakeAttribute2'
+        };
+        const options = Object.assign({}, defaultOptions, { attributes: givenAttributes });
+        const fakeAttributeQueryParameters = 'fakeattribute1=fakeAttribute1&fakeattribute2=fakeAttribute2';
+
+        const chatfuelEndpointUrl = `https://api.chatfuel.com/bots/${defaultBotId}/users/${defaultUserId}/send`;
+        const expectedUri = `${chatfuelEndpointUrl}?chatfuel_token=${defaultToken}&chatfuel_block_name=${defaultBlocklId}&${fakeAttributeQueryParameters}`;
+
+        broadcast(options);
+
+        expect(stubRequestPromisePost.getCall(0).args[0].uri).to.equal(expectedUri);
+    });
+});
