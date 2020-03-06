8452bd9d955f28527966324752a0a0092a140c7c
Marc Littlemore <marc.littlemore@gmail.com>
2020-03-06 14:37:45 +0000

    Remove deprecated Messenger message tags

--- src/validateFacebookTags.js
+++ src/validateFacebookTags.js
@@ -1,30 +1,5 @@
 // https://developers.facebook.com/docs/messenger-platform/send-messages/message-tags
 
-// TODO: Remove these from January 15th 2020
-const deprecatedFacebookTags = [
-    'BUSINESS_PRODUCTIVITY',
-    'COMMUNITY_ALERT',
-    'CONFIRMED_EVENT_REMINDER',
-    'NON_PROMOTIONAL_SUBSCRIPTION',
-    'PAIRING_UPDATE',
-    'APPLICATION_UPDATE',
-    'ACCOUNT_UPDATE',
-    'PAYMENT_UPDATE',
-    'PERSONAL_FINANCE_UPDATE',
-    'SHIPPING_UPDATE',
-    'RESERVATION_UPDATE',
-    'ISSUE_RESOLUTION',
-    'APPOINTMENT_UPDATE',
-    'GAME_EVENT',
-    'TRANSPORTATION_UPDATE',
-    'FEATURE_FUNCTIONALITY_UPDATE',
-    'TICKET_UPDATE',
-];
-
-const chatfuelTags = [
-    'UPDATE',
-];
-
 const validFacebookTags = [
     'CONFIRMED_EVENT_UPDATE',
     'POST_PURCHASE_UPDATE',
@@ -32,8 +7,6 @@ const validFacebookTags = [
     'HUMAN_AGENT',
 ];
 
-const allValidTags = [...deprecatedFacebookTags, ...validFacebookTags, ...chatfuelTags];
-
-const validateFacebookTags = tag => allValidTags.includes(tag);
+const validateFacebookTags = tag => validFacebookTags.includes(tag);
 
 export default validateFacebookTags;
