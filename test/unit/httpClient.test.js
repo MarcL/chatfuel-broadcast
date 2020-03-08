09fe70d7d0b1bd35827dbf67eff95fd78b5a3564
Marc Littlemore <marc.littlemore@gmail.com>
2020-03-08 13:42:58 +0000

    Add unit test for httpClient

---
+++ test/unit/httpClient.test.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import rateLimit from 'axios-rate-limit';
+
+jest.mock('axios-rate-limit');
+jest.mock('axios');
+
+describe('httpClient', () => {
+    it('should set expected Chatfuel rate limiting options', () => {
+        const fakeAxiosCreateResponse = {
+            get: () => {},
+        };
+        axios.create.mockReturnValue(fakeAxiosCreateResponse);
+
+        // eslint-disable-next-line global-require
+        require('../../src/httpClient');
+
+        expect(rateLimit).toHaveBeenCalledWith(fakeAxiosCreateResponse, { maxRPS: 25 });
+    });
+});
